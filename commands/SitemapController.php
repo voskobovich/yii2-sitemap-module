424b255900051bf2964a83c81a998dff84ebbbec
Vitaly Voskobovich <raficone@gmail.com>
2015-03-23 16:18:40 +0200

    Refactor code and update function

--- commands/SitemapController.php
+++ commands/SitemapController.php
@@ -29,19 +29,53 @@ class SitemapController extends Controller
     private $_baseDir = null;
 
     /**
+     * Location pages files
+     * @var string
+     */
+    private $_pageDir = null;
+
+    /**
+     * URI to sitemap file
+     * @var string
+     */
+    private $_pathSitemapFile = null;
+
+    /**
      * Init command
      */
     public function init()
     {
         $this->_module = Yii::$app->getModule('sitemap');
-        $this->_baseDir = Yii::getAlias('@webroot');
+        $this->_baseDir = Yii::getAlias('@app/../web');
+        $this->_pageDir = "{$this->_baseDir}/{$this->_module->pagesFolder}";
+        $this->_pathSitemapFile = "{$this->_baseDir}/{$this->_module->sitemapFileName}.xml";
+    }
+
+    /**
+     * List commands
+     */
+    public function actionIndex()
+    {
+        echo "sitemap/generate - Generate new sitemap\n";
+        echo "sitemap/delete   - Delete all files of sitemap\n";
+    }
+
+    /**
+     * Delete all files
+     */
+    public function actionDelete()
+    {
+        $this->deleteFiles();
     }
 
     /**
      * Building sitemat
      */
-    public function actionIndex()
+    public function actionGenerate()
     {
+        // Delete old files
+        $this->deleteFiles();
+
         // Get all links
         $siteMapData = $this->_module->buildSitemap();
 
@@ -66,23 +100,18 @@ class SitemapController extends Controller
 
         if ($pageCount > 1) {
             $pages = [];
-            $pagesFolder = "{$this->_baseDir}/{$this->_module->pagesFolder}";
-
-            // Clear old files
-            FileHelper::removeDirectory($pagesFolder);
-            FileHelper::createDirectory($pagesFolder, 0777);
 
             for ($page = 0; $page < $pageCount; $page++) {
                 $dataProvider->pagination->setPage($page);
                 $dataProvider->prepare(true);
 
-                $xmlData = Yii::$app->view->render(
+                $xmlData = Yii::$app->view->renderPhpFile(
                     $this->_module->viewPath . '/default/page-template.php',
                     ['urls' => $dataProvider->getModels()]
                 );
 
-                $fileName = "{$this->_module->sitemapFileName}{$page}.xml";
-                $filePath = "{$pagesFolder}/{$fileName}";
+                $fileName = "{$this->_module->pageFileName}{$page}.xml";
+                $filePath = "{$this->_pageDir}/{$fileName}";
 
                 if (file_put_contents($filePath, $xmlData)) {
                     $pages[] = [
@@ -101,24 +130,34 @@ class SitemapController extends Controller
     }
 
     /**
+     * Delete all files of sitemap
+     */
+    private function deleteFiles()
+    {
+        // Clear old files
+        FileHelper::removeDirectory($this->_pageDir);
+        FileHelper::createDirectory($this->_pageDir, 0777);
+
+        // Delete sitemap file
+        if (file_exists($this->_pathSitemapFile)) {
+            unlink($this->_pathSitemapFile);
+        }
+    }
+
+    /**
      * Writing data to a file
      * @param $urls
      * @param string $template
      */
-    public function putToFile($urls, $template = 'page-template')
+    private function putToFile($urls, $template = 'page-template')
     {
-        $fileName = "{$this->_baseDir}/{$this->_module->sitemapFileName}.xml";
-        if (file_exists($fileName)) {
-            unlink($fileName);
-        }
-
-        $xmlData = Yii::$app->view->render(
+        $xmlData = Yii::$app->view->renderPhpFile(
             $this->_module->viewPath . '/default/' . $template . '.php',
             ['urls' => $urls]
         );
 
-        if (file_put_contents($fileName, $xmlData)) {
-            echo "{$this->_module->sitemapFileName} - OK\n";
+        if (file_put_contents($this->_pathSitemapFile, $xmlData)) {
+            echo "{$this->_pathSitemapFile} - OK\n";
         }
     }
 }
